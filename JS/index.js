fbb8ceae5ad4764f70be5d2af729e85664a94389
vishudh <74951621+vishudhGupta@users.noreply.github.com>
2022-07-11 01:09:47 +0530

    Showing Results

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 
-
+let SCORE = 0;
 const handOptions = {
     "rock":"/assets/Rock.png",
     "paper":"/assets/Paper.png",
@@ -9,7 +9,7 @@ const handOptions = {
 
 //Creating a function for user choice 
 const pickUserHand = (hand)=>{
-    console.log(hand);
+    
 //onClick hide current page
 let hands = document.querySelector(".hands");
 hands.style.display = "none";
@@ -19,7 +19,7 @@ contest.style.display = "flex";
 //Set the User Picked 
 document.getElementById("userPickImage").src = handOptions[hand];
  let cpHand = pickComputerHand();
-
+referee(hand,cpHand)
 
 }
 const pickComputerHand = ()=> {
@@ -30,3 +30,46 @@ const pickComputerHand = ()=> {
 
 }
 
+const referee = (userHand, cpHand) => {
+    if (userHand == "paper" && cpHand == "scissors") {
+      setDecision("YOU LOSE!");
+    }
+    if (userHand == "paper" && cpHand == "rock") {
+      setDecision("YOU WIN!");
+      setScore(SCORE + 1);
+    }
+    if (userHand == "paper" && cpHand == "paper") {
+      setDecision("It's a tie!");
+    }
+    if (userHand == "rock" && cpHand == "scissors") {
+      setDecision("YOU WIN!");
+      setScore(SCORE + 1);
+    }
+    if (userHand == "rock" && cpHand == "paper") {
+      setDecision("YOU LOSE!");
+    }
+    if (userHand == "rock" && cpHand == "rock") {
+      setDecision("It's a tie!");
+    }
+    if (userHand == "scissors" && cpHand == "scissors") {
+      setDecision("It's a tie!");
+    }
+    if (userHand == "scissors" && cpHand == "rock") {
+      setDecision("YOU LOSE!");
+    }
+    if (userHand == "scissors" && cpHand == "paper") {
+      setDecision("YOU WIN!");
+      setScore(SCORE + 1);
+    }
+  };
+
+
+const setDecision = (decision) => {
+   document.querySelector(".decision h1").innerText = decision;
+}
+
+
+const setScore = (score) =>{
+    console.log(score);
+
+}
