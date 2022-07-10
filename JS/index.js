5ce919cb9752f30322f073c95c60c7d41858f0f4
vishudh <74951621+vishudhGupta@users.noreply.github.com>
2022-07-11 00:19:42 +0530

    USer hand pick jS

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,25 @@
 
+
+const handOptions = {
+    "rock":"/assets/Rock.png",
+    "paper":"/assets/Paper.png",
+    "scissors":"/assets/Scissors.png"    
+}
+
+
 //Creating a function for user choice 
 const pickUserHand = (hand)=>{
     console.log(hand);
 //onClick hide current page
 let hands = document.querySelector(".hands");
 hands.style.display = "none";
+// Show the Results
+let contest = document.querySelector(".contest")
+contest.style.display = "flex";
+//Set the User Picked 
+document.getElementById("userPickImage").src = handOptions[hand];
+ 
+
+
+
 }
-// Show the Next PAge 
