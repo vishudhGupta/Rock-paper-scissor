328e0842934ae4ce165f703fbeac9c6d87bad8b1
vishudh <74951621+vishudhGupta@users.noreply.github.com>
2022-07-11 00:49:46 +0530

    Computer Choice Automation complete

--- JS/index.js
+++ JS/index.js
@@ -18,8 +18,15 @@ let contest = document.querySelector(".contest")
 contest.style.display = "flex";
 //Set the User Picked 
 document.getElementById("userPickImage").src = handOptions[hand];
- 
-
+ let cpHand = pickComputerHand();
 
 
 }
+const pickComputerHand = ()=> {
+    let hands = ["rock","paper","scissors"]
+    let cpHand = hands[Math.floor(Math.random()*3)]
+    document.getElementById("computerPickImage").src = handOptions[cpHand];
+    return cpHand;
+
+}
+
