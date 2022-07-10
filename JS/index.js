f9f7491a8210b55a533ea2056e38f449d853aa1a
vishudh <74951621+vishudhGupta@users.noreply.github.com>
2022-07-10 23:57:55 +0530

    Completed Results Section

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+
+//Creating a function for user choice 
+const pickUserHand = (hand)=>{
+    console.log(hand);
+//onClick hide current page
+let hands = document.querySelector(".hands");
+hands.style.display = "none";
+}
+// Show the Next PAge 
